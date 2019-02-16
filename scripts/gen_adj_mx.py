92b803d969e156cfb81985a28d517e12492d66e7
Yossi Solomon <yosisolomon@gmail.com>
2019-02-17 01:25:32 +0200

    Change Adj.Mx. generator to work with latency CSV

--- scripts/gen_adj_mx.py
+++ scripts/gen_adj_mx.py
@@ -6,58 +6,66 @@ import argparse
 import numpy as np
 import pandas as pd
 import pickle
+from networkx import Graph, algorithms
+from collections import namedtuple
+
+Link = namedtuple('Link', 'From, To, Latency_in_ms')
 
 
-def get_adjacency_matrix(distance_df, sensor_ids, normalized_k=0.1):
+def get_adjacency_matrix(links, switches, normalized_k):
     """
-
-    :param distance_df: data frame with three columns: [from, to, distance].
-    :param sensor_ids: list of sensor ids.
+    :param links: data frame with three columns: [from, to, distance].
+    :param switches: list of switches.
     :param normalized_k: entries that become lower than normalized_k after normalization are set to zero for sparsity.
     :return:
     """
-    num_sensors = len(sensor_ids)
-    dist_mx = np.zeros((num_sensors, num_sensors), dtype=np.float32)
-    dist_mx[:] = np.inf
     # Builds sensor id to index map.
-    sensor_id_to_ind = {}
-    for i, sensor_id in enumerate(sensor_ids):
-        sensor_id_to_ind[sensor_id] = i
+    switch_to_ind = {}
+    for i, sensor_id in enumerate(switches):
+        switch_to_ind[sensor_id] = i
+    # Graph is not directed as the links are assumed to be symmetric
+    network = Graph()
+    [network.add_node(switch) for switch in switches]
+    for link in links.values:
+        network.add_edge(link[0], link[1], latency_in_ms=link[2])
 
-    # Fills cells in the matrix with distances.
-    for row in distance_df.values:
-        if row[0] not in sensor_id_to_ind or row[1] not in sensor_id_to_ind:
-            continue
-        dist_mx[sensor_id_to_ind[row[0]], sensor_id_to_ind[row[1]]] = row[2]
+    dist_mx = np.zeros((len(switches), len(switches)), dtype=np.float32)
+    dist_mx[:] = np.inf
+    # Fills cells in the matrix with latencies
+    latencies = dict(algorithms.shortest_path_length(network, weight='latency_in_ms'))
+    for i in switch_to_ind.keys():
+        for j in switch_to_ind.keys():
+            if j not in latencies[i]:
+                print('jumping i={0} j={1}'.format(i, j))
+                continue
+            dist_mx[switch_to_ind[i]][switch_to_ind[j]] = latencies[i][j]
 
     # Calculates the standard deviation as theta.
     distances = dist_mx[~np.isinf(dist_mx)].flatten()
     std = distances.std()
     adj_mx = np.exp(-np.square(dist_mx / std))
-    # Make the adjacent matrix symmetric by taking the max.
-    # adj_mx = np.maximum.reduce([adj_mx, adj_mx.T])
 
     # Sets entries that lower than a threshold, i.e., k, to zero for sparsity.
     adj_mx[adj_mx < normalized_k] = 0
-    return sensor_ids, sensor_id_to_ind, adj_mx
+    print(adj_mx)
+    return adj_mx
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--sensor_ids_filename', type=str, default='data/sensor_graph/graph_sensor_ids.txt',
-                        help='File containing sensor ids separated by comma.')
-    parser.add_argument('--distances_filename', type=str, default='data/sensor_graph/distances_la_2012.csv',
+    parser.add_argument('--links_csv', type=str, default='data/sensor_graph/Bellcanada.graphml.csv',
                         help='CSV file containing sensor distances with three columns: [from, to, distance].')
     parser.add_argument('--normalized_k', type=float, default=0.1,
-                        help='Entries that become lower than normalized_k after normalization are set to zero for sparsity.')
-    parser.add_argument('--output_pkl_filename', type=str, default='data/sensor_graph/adj_mat.pkl',
+                        help='Entries lower than normalized_k after normalization are set to zero for sparsity.')
+    parser.add_argument('--output_pkl_filename', type=str, default='data/sensor_graph/Bellcanada.adj_mat.pkl',
                         help='Path of the output file.')
     args = parser.parse_args()
 
-    with open(args.sensor_ids_filename) as f:
-        sensor_ids = f.read().strip().split(',')
-    distance_df = pd.read_csv(args.distances_filename, dtype={'from': 'str', 'to': 'str'})
-    _, sensor_id_to_ind, adj_mx = get_adjacency_matrix(distance_df, sensor_ids)
+    links_df = pd.read_csv(args.links_csv, dtype={'From': 'str', 'To': 'str'})
+    switches = set([link[0] for link in links_df.values] + [link[1] for link in links_df.values])
+    # make the result reproducible by making it sorted
+    switches = sorted(switches)
+    adj_mx = get_adjacency_matrix(links_df, switches, args.normalized_k)
     # Save to pickle file.
     with open(args.output_pkl_filename, 'wb') as f:
-        pickle.dump([sensor_ids, sensor_id_to_ind, adj_mx], f, protocol=2)
+        pickle.dump(adj_mx, f, protocol=2)
