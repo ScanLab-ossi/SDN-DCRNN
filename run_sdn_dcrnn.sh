43d72ba9a59d74a1874014594e33742ef7ab2788
Yossi Solomon <yosisolomon@gmail.com>
2019-09-14 16:14:28 +0300

    fixing path generation in automation script

--- run_sdn_dcrnn.sh
+++ run_sdn_dcrnn.sh
@@ -17,14 +17,14 @@ fi
 # generate_training_data + HD5 ==> data npz
 python scripts/generate_training_data.py --traffic_df_filename=$EXP_DIR/sflow-datagrams.hd5 --output_dir=$EXP_DIR
 # gen_adj_mx + csv ==> adj mx pkl
-LINKS_CSV=`echo $EXP_DIR/*.graphml.csv`
+LINKS_CSV=`ls $EXP_DIR/*.graphml-topo.csv`
 python scripts/gen_adj_mx.py --links_csv=$LINKS_CSV  --intfs_list=$EXP_DIR/intfs-list
 # gen_config + paths + nodes num ==> config file
-GRAPH_PKL=`echo $EXP_DIR/*.pkl`
+GRAPH_PKL=`ls $EXP_DIR/*.pkl`
 PORT_NUM=`cat $EXP_DIR/*.port.num`
 python scripts/gen_config.py --dataset_dir=$EXP_DIR --graph_adj_mx_pkl=$GRAPH_PKL --num_ports=$PORT_NUM
 # dcrnn_train + config file ==> model
-CONFIG_FILE=`echo $EXP_DIR/*.yaml`
+CONFIG_FILE=`ls $EXP_DIR/*.yaml`
 python dcrnn_train.py --config_file=$CONFIG_FILE
 # run_demo + config file ==> new predictions
 PREDICTIONS_FILE=$EXP_DIR/predictions.npz
