0dd29c3a174c6db5bd138ef4cd0803047dac92cc
Yossi Solomon <yosisolomon@gmail.com>
2019-12-05 00:37:35 +0200

    Running SDN-DCRNN in parallel for multiple datasets

--- run_sdn_dcrnn_multi.sh
+++ run_sdn_dcrnn_multi.sh
@@ -10,13 +10,17 @@ exit_with_msg() {
 }
 
 if [ -z "$EXP_DATA_PATH" ] ; then
-	  exit_with_msg "The variable EXP_DATA_PATH must be set to the storage path containing data directories for analysis!"
+	exit_with_msg "The variable EXP_DATA_PATH must be set to the storage path containing data directories for analysis!"
 fi
 
-ROOT_DIR=$(dirname "${BASH_SOURCE[0]}")
+if [ -z "$PARALLEL" ] ; then
+	PARALLEL=4
+	echo "The variable PARALLEL was not set, default=$PARALLEL will be used for parallelization"
+else
+	echo "The variable PARALLEL was set to $PARALLEL and will be used for parallelization"
+fi
 
-for EXP_DIR in `ls -d $EXP_DATA_PATH/*/` ; do
-  echo Running SDN-DCRNN for directory: $EXP_DIR
-  export EXP_DIR
-  "$ROOT_DIR"/run_sdn_dcrnn.sh
-done
+export ROOT_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+ls -d $EXP_DATA_PATH/*/ | parallel -j $PARALLEL 'export EXP_DIR={} ; echo Running SDN-DCRNN for directory: $EXP_DIR ; $ROOT_DIR/run_sdn_dcrnn.sh'
+
